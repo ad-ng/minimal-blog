17b89e90c54f404f892e21f78136e44db3b38929
NGOGA Adolphe <adolphengoga@gmail.com>
2024-11-18 08:36:54 +0200

    getting current user data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService){}
+        @Get('me')
+        async current(){
+            return await this.userService.getCurrentUser(5)
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma: PrismaService){}
+    async getCurrentUser(id){
+        const user:object = await this.prisma.users.findUnique({
+            where: { id }
+        })
+        return user
+    }
+}
