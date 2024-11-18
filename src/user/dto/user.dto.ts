db8daea3789093398f05935a264045db61962058
NGOGA Adolphe <adolphengoga@gmail.com>
2024-11-18 08:56:27 +0200

    validating role enum

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,11 +1,23 @@
-export class UserDto{
-    name: string
-    email: string
-    password: string
-    phoneNumber: string
-    role: Roles
-}
+import { IsEnum, IsString } from "class-validator"
+
 enum Roles{
-    admin,
-    author
+    ADMIN = 'admin',
+    USER = 'user',
+}
+
+export class UserDto{
+    @IsString()
+    name: string
+
+    @IsString()
+    email: string
+
+    @IsString()
+    password: string
+
+    @IsString()
+    phoneNumber: string
+
+    @IsEnum(Roles, { message: 'role can only be admin or author'})
+    role: Roles
 }
