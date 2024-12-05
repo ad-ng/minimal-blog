ee3236d222116865fdb131eeeb20404dacb40e04
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-05 10:07:08 +0200

    update user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get,  Req,  UseGuards } from '@nestjs/common';
+import { Body, Controller, Get,  Put,  Req,  UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService){}
-
-    @UseGuards(AuthGuard('jwt'))
+    
     @Get('me')
     async current(@Req() req: Request){
         return await this.userService.getCurrentUser(req.user)
+    }
+
+    @Put('me')
+    async updateUser(@Body() dto:any,@Req() req: Request){
+        return this.userService.updateMe(req.user, dto)
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2'
 
 @Injectable()
 export class UserService {
@@ -9,5 +11,31 @@ export class UserService {
             where: { id: current.sub }
         })
         return user
+    }
+
+    async updateMe(current, dto){
+        const hashedPW = await argon.hash(dto.password)
+        try {
+            const user = await this.prisma.users.update({
+                where: { email: current.email },
+                data: {
+                    names: dto.names,
+                    email: dto.email,
+                    password: hashedPW
+                    }
+            })
+            delete user.id
+            return {
+                message: 'data updated successfully',
+                data: user
+            }
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){
+                if(error.code == 'P2002'){
+                    throw new BadRequestException('email already taken')
+                }
+            }
+        }
+
     }
 }
