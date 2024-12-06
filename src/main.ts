c667c8db2de8be73e1b05c61b0422c57c214da54
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-06 09:31:44 +0200

    initialising openapi

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,14 @@ async function bootstrap() {
       whitelist: true
     })
   )
+  const config = new DocumentBuilder()
+    .setTitle('minimal blog')
+    .setDescription('About this is a minimal blog backend built in node, Nest.js as a framework, Prisma as ORM and Postgres for database')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
