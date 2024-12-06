76fc9c69e74a1e66ba908bda212215cd6a21698a
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-06 07:50:57 +0200

    get all categories

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(private prisma: PrismaService){}
+
+    async allCategories(){
+        const cat: object[] = await this.prisma.category.findMany()
+        return {
+            message: 'categories found successfully',
+            data: cat
+        }
+    }
+}
