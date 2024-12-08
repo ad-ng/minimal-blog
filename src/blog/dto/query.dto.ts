031adf71c88ed0636d5905dca04a6bd950432c0b
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-08 10:54:13 +0200

    documenting pagination

--- src/blog/dto/query.dto.ts
+++ src/blog/dto/query.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsInt, IsOptional, Min } from "class-validator";
 
 export class BlogQueryDto {
+    @ApiProperty()
     @IsOptional()
     @IsInt()
     @Min(1)
     @Type(()=>Number)
     page: number;
 
+    @ApiProperty()
     @IsOptional()
     @IsInt()
     @Min(1)
